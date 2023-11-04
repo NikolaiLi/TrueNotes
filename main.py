ec8c3c0e9419c27c9881202b46a9e6ae1779622a
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-11-04 22:45:21 +0100

    Ting ting

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ app = Sanic("Hej")
 app.static("/static", "./static")
 jinja = SanicJinja2(app)
 
-globals = {}
 
 @app.get("/")
 @jinja.template("index.html")
 async def index(request):
     return None
 
-@app.post("/add")
-async def add_number(request):
-     return redirect(f"/")
+@app.get("/noter")
+@jinja.template("noter.html")
+async def noter(request):
+    return None
+
+@app.get("/opret")
+@jinja.template("opret.html")
+async def opret(request):
+    return None
+
+@app.post("/tilnoter")
+async def til_noter(request):
+     return redirect("/noter")
+
+@app.post("/tilstart")
+async def til_start(request):
+    return redirect("/")
+
+@app.post("/tilopret")
+async def til_opret(request):
+    return redirect("/opret")
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080)
+    app.run(host="localhost", port=8080, debug = True)

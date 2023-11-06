49b3caf522a548134cc9d063c80a05a11c446b9e
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-11-06 11:01:05 +0100

    Stor ting

--- main.py
+++ main.py
@@ -17,22 +17,12 @@ async def index(request):
 async def noter(request):
     return None
 
+
 @app.get("/opret")
 @jinja.template("opret.html")
 async def opret(request):
     return None
 
-@app.post("/tilnoter")
-async def til_noter(request):
-     return redirect("/noter")
-
-@app.post("/tilstart")
-async def til_start(request):
-    return redirect("/")
-
-@app.post("/tilopret")
-async def til_opret(request):
-    return redirect("/opret")
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug = True)

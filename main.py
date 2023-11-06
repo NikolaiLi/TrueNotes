92b608cfa7fe73aa66d85e2ec7cbf99ba9dc3663
Ball-slobber1 <bertrambendsen.bb@gmail.com>
2023-11-06 12:41:58 +0100

    sjjs

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ app = Sanic("Hej")
 app.static("/static", "./static")
 jinja = SanicJinja2(app)
 
+globals = {"menu": {"Startside":"/", "Opret": "/opret", "Noter": "/noter"}}
 
 @app.get("/")
 @jinja.template("index.html")
 async def index(request):
-    return None
+    return globals
 
 @app.get("/noter")
 @jinja.template("noter.html")
 async def noter(request):
-    return None
-
+    return globals
 
 @app.get("/opret")
 @jinja.template("opret.html")
 async def opret(request):
-    return None
+    return globals
 
 
 if __name__ == "__main__":

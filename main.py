e128cc04c0d5b38157063452c33f34621311f4e5
NikolaiLi <122288063+NikolaiLi@users.noreply.github.com>
2023-11-06 12:44:51 +0100

    /ny

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sanic import Sanic
 from sanic.response import redirect 
 from sanic_jinja2 import SanicJinja2
+from model.post import Post
+import uuid
 
 app = Sanic("Hej")
 app.static("/static", "./static")
@@ -23,6 +25,18 @@ async def noter(request):
 async def opret(request):
     return globals
 
+@app.post("/ny")
+async def ny(request):
+    title = request.form.get('Navn')
+    text = request.form.get('Tekst')
+    id = str(uuid.uuid4())
+
+    newpost = Post(title,text)
+
+    globals["posts"][id] = newpost
+    print(title + text)
+    
+    return redirect("/noter")
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug = True)

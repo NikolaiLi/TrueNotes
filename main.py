d8edb6a17164a030a68e458398487840ae3900f5
Ata12333 <118903521+Ata12333@users.noreply.github.com>
2023-11-13 08:24:54 +0100

    Oprettelse af noter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sanic import Sanic
-from sanic.response import redirect 
+from sanic.response import redirect
 from sanic_jinja2 import SanicJinja2
 from model.post import Post
 import uuid
@@ -8,29 +8,43 @@ app = Sanic("Hej")
 app.static("/static", "./static")
 jinja = SanicJinja2(app)
 
+notes = []
+
 globals = {"menu": {"Startside":"/", "Opret": "/opret", "Noter": "/noter", "Log Ind": "/logind"},
            "posts": {}
            }
 
-@app.get("/")
+@app.get("/", name = "index")
 @jinja.template("index.html")
 async def index(request):
-    return globals
+    return {"menu": {"Startside": "/", "Opret": "/opret", "Noter": "/noter", "Log Ind": "/logind"}, "notes": notes}
 
-@app.get("/noter")
+@app.get("/noter", name = "noter_page")
 @jinja.template("noter.html")
 async def noter(request):
-    return globals
+    return {"menu": {"Startside": "/", "Opret": "/opret", "Noter": "/noter", "Log Ind": "/logind"}, "notes": notes}
 
-@app.get("/opret")
+@app.get("/opret", name = "opret")
 @jinja.template("opret.html")
 async def opret(request):
-    return globals
+    return {"menu": {"Startside": "/", "Opret": "/opret", "Noter": "/noter", "Log Ind": "/logind"}}
+
+@app.post("/Note")
+async def Note(request):
+    title = request.form.get('Emne')
+    text = request.form.get('Note')
+    id = str(uuid.uuid4())
+
+    note = {"id": id, "title": title, "text": text}
+
+    notes.append(note)
+
+    return redirect("/noter")
 
 @app.get("/logind")
 @jinja.template("logind.html")
 async def logind(request):
-    return globals
+    return {"menu": {"Startside": "/", "Opret": "/opret", "Noter": "/noter", "Log Ind": "/logind"}}
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug = True)
